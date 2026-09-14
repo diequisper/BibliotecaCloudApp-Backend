2e44046e426c81b5b03ec71ae09a16dfca718a0b
diequisper <diequisper@gmail.com>
2026-09-14 13:41:52 -0500

    fix 140926

--- EF-DiegoQuispeR/Startup.cs
+++ EF-DiegoQuispeR/Startup.cs
@@ -35,7 +35,7 @@ namespace EF_DiegoQuispeR
             {
                 options.AddPolicy("AngularPlcy", builder =>
                 {
-                    builder.WithOrigins("http://localhost:4200")
+                    builder.AllowAnyOrigin()
                         .AllowAnyMethod()
                         .AllowAnyHeader();
                 });
