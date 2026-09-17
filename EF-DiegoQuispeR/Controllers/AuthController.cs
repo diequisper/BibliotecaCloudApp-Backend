d36fa6b039e16f97a60c340a6a882ceb665ebf3d
diequisper <diequisper@gmail.com>
2026-09-17 17:11:32 -0500

    just removing unused library

--- EF-DiegoQuispeR/Controllers/AuthController.cs
+++ EF-DiegoQuispeR/Controllers/AuthController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
